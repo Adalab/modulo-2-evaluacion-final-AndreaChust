707129b8f6cf96b33f392fd8798cd9374e7ff190
AndreaChust <andrea.chust@hotmail.com>
2025-03-14 14:40:17 +0100

    favorites

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const search = document.querySelector(".js-input");
 const button = document.querySelector(".js-btn");
 const reset = document.querySelector(".js-reset");
 const list = document.querySelector(".js-list");
+const favoriteAnime = document.querySelector(".js-favorite-anime");
 
 let animeList = []; //Creo un array vacío que luego llenare con los datos del servidor, linea 20.
 let animeFavoritesList = [];
@@ -18,7 +19,7 @@ const handleSearch = (ev) => {
         .then(data => {
             const animes = data.data; //accedo a la propiedad de mi objeto
             animeList = animes; //Lleno mi array vacío
-            list.innerHTML = "";
+            list.innerHTML = ""; //Vacío mi lista antes para no repetir animes
             for (const anime of animes) {
                 let image = anime.images.jpg.image_url; //accedo a la url de las imagenes
                 const title = anime.title;
@@ -45,9 +46,27 @@ const handleSearch = (ev) => {
 button.addEventListener("click", handleSearch);
 
 function handleAddFavorite(ev){
-    const idCartoon = ev.currentTarget.id;
+    const idCartoon = ev.currentTarget.id; // Aquí es .id porque el atributo es id.
     const animeSelected = animeList.find((anime) => {
-        return anime.mal_id === idCartoon;
+        return anime.mal_id === parseInt(idCartoon); //Como currenTarget.id es un string y anime.mal_id es un numero va a dar undefined, por lo que tengo que convertir el currentTargert a numero.
+        
     })
-    
+
+    animeFavoritesList.push(animeSelected);
+
+        favoriteAnime.innerHTML = "";
+    for (const anime of animeFavoritesList) {
+        let image = anime.images.jpg.image_url; //accedo a la url de las imagenes
+        const title = anime.title;
+        const withoutImage = "https://cdn.myanimelist.net/img/sp/icon/apple-touch-icon-256.png";
+        const placeHolder = "https://media.istockphoto.com/id/1409329028/vector/no-picture-available-placeholder-thumbnail-icon-illustration-design.jpg?s=612x612&w=0&k=20&c=_zOuJu755g2eEUioiOUdz_mHKJQJn-tDgIAhQzyeKUQ=";
+        if (image === withoutImage) {
+            image = placeHolder
+        }
+        favoriteAnime.innerHTML += `<li class="js-list-anime" id=${anime.mal_id}> 
+        <img src="${image}" alt="Imagen del anime ${title}">
+        <h3>${title}</h3>
+        </li>`
+    }
 }
+
